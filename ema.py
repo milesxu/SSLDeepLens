6532b7919311fac30ed7858371e893984367a57c
Miles Xu <milesxu@outlook.com>
2019-01-10 23:54:59 +0800

    ema rewrite, update directly

--- ema.py
+++ ema.py
@@ -1,18 +1,20 @@
 import torch.nn as nn
+import copy
 
 
-class EMA(nn.Module):
-    def __init__(self, mu):
+class EMA:
+    def __init__(self, mu, net: nn.Module):
         super(EMA, self).__init__()
         self.mu = mu
-        self.shadow = {}
-
-    def register(self, name, val):
-        self.shadow[name] = val.clone()
+        self.src = net
+        self.cpy = copy.deepcopy(net)
 
     # use the formula in https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
-    def forward(self, name, x):
-        assert name in self.shadow
-        # new_avg = self.mu * x + (1.0 - self.mu) * self.shadow[name]
-        self.shadow[name] -= (1 - self.mu) * (self.shadow[name] - x)
-        return new_avg
+    def update(self):
+        for s_param, c_param in \
+                zip(self.src.parameters(), self.cpy.parameters()):
+            if s_param.require_grad:
+                c_param.data -= (1 - self.mu) * (c_param.data - s_param.data)
+
+    def __call__(self, x):
+        return self.cpy(x)
