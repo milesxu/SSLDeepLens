01712731fc7179631541e9ef15eed660b52a9921
Miles Xu <milesxu@outlook.com>
2019-03-05 15:17:20 +0800

    change detail loss and acc record

--- run_loop.py
+++ run_loop.py
@@ -23,6 +23,7 @@ class SNTGRunLoop(object):
         self.device = device
         self.net.to(device)
         n_data, num_classes = params['n_data'], params['num_classes']
+        n_eval_data, batch_size = params['n_eval_data'], params['batch_size']
         self.ensemble_pred = torch.zeros((n_data, num_classes), device=device)
         self.target_pred = torch.zeros((n_data, num_classes), device=device)
         t_one = torch.ones(())
@@ -30,8 +31,18 @@ class SNTGRunLoop(object):
             (n_data, num_classes), dtype=torch.float32, device=device)
         self.epoch_mask = t_one.new_empty(
             (n_data), dtype=torch.float32, device=device)
-        self.epoch_loss = t_one.new_empty((n_data // params['batch_size'], 4),
-                                          dtype=torch.float32, device=device)
+        self.train_epoch_loss = \
+            t_one.new_empty((n_data // batch_size, 4),
+                            dtype=torch.float32, device=device)
+        self.train_epoch_acc = \
+            t_one.new_empty((n_data // batch_size), dtype=torch.float32,
+                            device=device)
+        self.eval_epoch_loss = \
+            t_one.new_empty((n_eval_data // batch_size, 2),
+                            dtype=torch.float32, device=device)
+        self.eval_epoch_acc = \
+            t_one.new_empty((n_eval_data // batch_size, 2),
+                            dtype=torch.float32, device=device)
         self.optimizer = opt.Adam(self.net.parameters())
         self.update_fn = update_fn
         self.ema = EMA(params['polyak_decay'], self.net, has_cuda)
@@ -88,17 +99,15 @@ class SNTGRunLoop(object):
                 # loss = F.binary_cross_entropy_with_logits(outputs, one_hot)
                 # print(loss.item())
 
-                train_acc = torch.mean(torch.argmax(
+                self.train_epoch_acc[i] = torch.mean(torch.argmax(
                     outputs[labeled_mask], 1).eq(is_lens[labeled_mask]).float())
                 # train_acc = torch.mean(
                 #     torch.argmax(outputs, 1).eq(is_lens).float())
-                train_accs.append(train_acc)
-                # print(loss.item())
-                self.epoch_loss[i, 0] = loss.item()
+                self.train_epoch_loss[i, 0] = loss.item()
 
                 # unlabeled loss
                 unlabeled_loss = torch.mean((predicts - targets)**2)
-                self.epoch_loss[i, 1] = unlabeled_loss.item()
+                self.train_epoch_loss[i, 1] = unlabeled_loss.item()
                 loss += unlabeled_loss * self.unsup_weight
 
                 # SNTG loss
@@ -115,10 +124,10 @@ class SNTGRunLoop(object):
                                          torch.zeros_like(eucd))
                     embed_losses = torch.where(neighbor_bool, eucd2, eucd_y)
                     embed_loss = torch.mean(embed_losses)
-                    self.epoch_loss[i, 2] = embed_loss.item()
+                    self.train_epoch_loss[i, 2] = embed_loss.item()
                     loss += embed_loss * \
                         self.unsup_weight * self.params['embed_coeff']
-                self.epoch_loss[i, 3] = loss.item()
+                self.train_epoch_loss[i, 3] = loss.item()
                 loss.backward()
                 self.optimizer.step()
                 self.ema.update()
@@ -128,8 +137,10 @@ class SNTGRunLoop(object):
                 (1 - self.params['pred_decay']) * self.epoch_pred
             self.targets_pred = self.ensemble_pred / \
                 (1.0 - self.params['pred_decay'] ** (epoch + 1))
-            loss_mean = torch.mean(self.epoch_loss, 0)
+            loss_mean = torch.mean(self.train_epoch_loss, 0)
             train_losses.append(loss_mean[3].item())
+            acc_mean = torch.mean(self.train_epoch_acc)
+            train_accs.append(acc_mean.item())
             print(f"epoch {epoch}, time cosumed: {time.time() + train_time}, "
                   f"labeled loss: {loss_mean[0].item()}, "
                   f"unlabeled loss: {loss_mean[1].item()}, "
@@ -147,31 +158,33 @@ class SNTGRunLoop(object):
                         data_batched['is_lens']
                     # currently h_x in evalization is not used
                     eval_logits, _ = self.ema(images)
-                    ema_eval_acc = torch.mean(torch.argmax(
+                    self.eval_epoch_acc[i, 0] = torch.mean(torch.argmax(
                         eval_logits, 1).eq(is_lens).float())
-                    ema_eval_accs.append(ema_eval_acc.item())
                     # print(f"ema evaluation accuracy: {ema_eval_acc.item()}")
                     eval_lens = torch.zeros(
                         len(is_lens), is_lens.max()+1,
                         device=self.device) \
                         .scatter_(1, is_lens.unsqueeze(1), 1.)
                     # eval_loss = self.loss_fn(eval_logits, is_lens)
-                    ema_eval_loss = F.binary_cross_entropy_with_logits(
+                    self.eval_epoch_loss[i, 0] = \
+                        F.binary_cross_entropy_with_logits(
                         eval_logits, eval_lens)
-                    ema_eval_losses.append(ema_eval_loss.item())
                     # break
                     eval_logits, _ = self.net(images)
-                    eval_acc = torch.mean(torch.argmax(
+                    self.eval_epoch_acc[i, 1] = torch.mean(torch.argmax(
                         eval_logits, 1).eq(is_lens).float())
-                    eval_accs.append(eval_acc.item())
                     # print(f"evaluation accuracy: {eval_acc.item()}")
-                    eval_loss = F.binary_cross_entropy_with_logits(
+                    self.eval_epoch_loss[i, 1] = \
+                        F.binary_cross_entropy_with_logits(
                         eval_logits, eval_lens)
-                    eval_losses.append(eval_loss.item())
-                print(f"ema accuracy: "
-                      f"{torch.mean(torch.tensor(ema_eval_accs)).item()}, "
-                      f"normal accuracy: "
-                      f"{torch.mean(torch.tensor(eval_accs)).item()}")
+                loss_mean = torch.mean(self.eval_epoch_loss, 0)
+                acc_mean = torch.mean(self.eval_epoch_acc, 0)
+                ema_eval_accs.append(acc_mean[0].item())
+                ema_eval_losses.append(loss_mean[0].item())
+                eval_accs.append(acc_mean[1].item())
+                eval_losses.append(loss_mean[1].item())
+                print(f"ema accuracy: {acc_mean[0].item()}"
+                      f"normal accuracy: {acc_mean[1].item()}")
 
         return train_losses, train_accs, eval_losses, eval_accs, \
             ema_eval_losses, ema_eval_accs
